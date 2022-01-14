82c91ab8542757126882bd10819fae8d14f97754
berpl <eder.cornejo.diaz@ucsp.edu.pe>
2022-01-14 00:15:05 -0500

    Updates

---
+++ precache-manifest.41dbcc25ee7bd0a5c16ddc874715da92.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebb055163bf38090fdfefd5b01d477a2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fb4b227f68c300645d1a",
+    "url": "/static/js/2.57853e6f.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.57853e6f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b4b3cdd36e7a42f02dd",
+    "url": "/static/js/main.01f2e03d.chunk.js"
+  },
+  {
+    "revision": "492b4823c3187921208c",
+    "url": "/static/js/runtime-main.4a991839.js"
+  }
+]);
